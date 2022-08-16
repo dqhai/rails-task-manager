816320f454460c445fb4a10d6a24ffe996bfb218
Q Hai Dang <haiqd@pm.me>
2022-08-16 15:23:49 +0200

    create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,26 @@ class TasksController < ApplicationController
     # @task = @tasks.find(params[:id])
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+  # def edit
+
+  # end
+
+  # def update
+
+  # end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # what changes when we put it before show/details route
   get "tasks/:id", to: "tasks#details", as: :task
+  # show vs details, aberrating from convention problem?
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
 end
 
 # it works without doing 'Add _path to the route prefix (left column)'?
+# What are the prefixes?
